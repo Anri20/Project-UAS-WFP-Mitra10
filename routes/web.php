2cf277a5e77fdff5e4035b337a41f854a6315804
Anri20 <88647934+Anri20@users.noreply.github.com>
2023-11-02 16:28:53 +0700

    delete

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,6 @@ Route::resource('brand', 'BrandController');
 Route::resource('category', 'CategoryController');
 Route::resource('review', 'ReviewController');
 
-<<<<<<< HEAD
 Route::get('/conquer', function () {
     return view('layout.conquer');
 });
@@ -43,27 +42,10 @@ Route::resource('productstock', ProductStockController::class);
 Route::resource('promos', PromosController::class);
 Route::resource('report', ReportController::class);
 Route::resource('reportjumlah', ReportproductController::class);
-=======
+
 Route::resource('shop', 'ShopController');
 // Route::resource('shoparea', 'ShopAreaController');
 Route::resource('productstock', 'ProductStockController');
 Route::resource('promos', 'PromosController');
 
 Route::resource('transactions', 'TransactionController');
-
-// ===============================================================================
-
-Route::get('/dashboard/week6', 'ProductController@week6')->name('dashboard.week6');
-Route::get('/dashboard/stock/week6', 'ProductController@week6Stock')->name('dashboard.stock.week6');
-
-Route::get('/category/tugas/week4', 'CategoryController@week4')->name('categoryweek4');
-// Route::get('/category/tugas/week5', 'CategoryController@week5')->name('categoryweek5');
-Route::get('/report/category/avgprice', 'CategoryController@reportCatAvgPrice')->name('reportCatAvgPrice');
-Route::get('/category/tugas/week6', 'CategoryController@week6')->name('category.week6');
-Route::get('/category/stock/week6', 'CategoryController@week6Stock')->name('category.stock.week6');
-
-Route::get('/brand/tugas/week4', 'BrandController@week4')->name('brandweek4');
-Route::get('/brand/tugas/week5', 'BrandController@week5')->name('brandweek5');
-Route::get('/brand/tugas/week6', 'BrandController@week6')->name('brand.week6');
-Route::get('/brand/stock/week6', 'BrandController@week6Stock')->name('brand.stock.week6');
->>>>>>> f4534375d880b7eb7ef005fa13c4248bde1a34b5
